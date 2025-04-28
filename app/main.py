0ddee774b2b2337ec67e249bc6b8c3e412a71a13
Leeroy Garcia <leeroy.uziel.gg@outlook.es>
2025-04-27 20:22:12 -0600

    first commit, conf and add endpoint

---
+++ app/main.py
@@ -0,0 +1,51 @@
+import email
+from typing import List, Optional, Union
+
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from fastapi_mail import ConnectionConfig, FastMail, MessageSchema, MessageType
+from pydantic import BaseModel, EmailStr
+
+app = FastAPI()
+
+class EmailSchema(BaseModel):
+    name: str
+    email: Optional[str]
+    phone: Optional[str]
+    message: str
+
+conf = ConnectionConfig(
+    MAIL_USERNAME ="sukaritas19@gmail.com",
+    MAIL_PASSWORD = "ypglzvhepdwrrjxp",
+    MAIL_FROM = "sukaritas19@gmail.com",
+    MAIL_PORT = 587,
+    MAIL_SERVER = "smtp.gmail.com",
+    MAIL_STARTTLS = True,
+    MAIL_SSL_TLS = False,
+    USE_CREDENTIALS = True,
+    VALIDATE_CERTS = True
+)
+
+# html = """
+# <h3>Nombre: ${name}</h3>
+# <h3>Email: ${email}</h3>
+# <br/>
+# <p>Correo electronico: ${message}</p>
+# """
+    
+@app.post("/send-email")
+async def read_root(emailBody: EmailSchema) -> JSONResponse:
+    print("emial???????????????",emailBody.email)
+    message = MessageSchema(
+        subject="Fastapi-Mail module",
+        recipients=["leeroy.uziel.gg@outlook.es"],
+        body=f"""<h2>Email: {emailBody.email}</h1>
+                <h2>Name: {emailBody.name} </h2>
+                <h2>Phone: {emailBody.phone} </h2>
+                <p>Message: {emailBody.message} </p>
+        """,
+        subtype=MessageType.html)
+    fm = FastMail(conf)
+    await fm.send_message(message)
+    return JSONResponse(status_code=200, content={"message": "email has been sent"}) 
+
